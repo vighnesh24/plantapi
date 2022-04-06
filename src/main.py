7f28e330b22d6efa85679e668e3ea3b3a782fb81
vighnesh24 <75435281+vighnesh24@users.noreply.github.com>
2022-04-06 14:22:21 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from torch.functional import split
-from model_files.ml_predict import predict_plant, Network
+from src.model_files.ml_predict import predict_plant, Network
 from pyfcm import FCMNotification
 import base64
-from decouple import config
+# from decouple import config
 
 
 app = Flask("Plant Disease Detector")
 CORS(app)
-push_service = FCMNotification(api_key=config('API_KEY'))
+push_service = FCMNotification(api_key="<api-key>")
+@app.route('/', methods=['GET'])
+def index():
+    return "PLANT APP API"
 
-@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    key_dict = request.get_json()
+    key_dict = request.get_json(force=True)
     image = key_dict["image"]
     imgdata = base64.b64decode(image)
     model = Network()
